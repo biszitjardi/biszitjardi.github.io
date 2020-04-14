095af03b99cf1f77124cd9d6fafdc106b16c2bde
biszitjardi <57011633+biszitjardi@users.noreply.github.com>
2020-04-14 14:37:09 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Gavin demali Homepage</title>
+		<meta name="viewport" content="width=devide-width, initial-scale=1">
+		<link href='https://fonts.googleapis.com/css?family=Faster One' rel='stylesheet'>
+		<link href='style.css' rel='stylesheet'>
+	</head>
+	<body>
+		<ul id='navbar'>
+			<!-- Logo | Home | Forum | Porn | Donate (go to same page) | math -->
+			<li class='logohome'>
+				<a class='navlink' href='https://biszitjardi.github.io'>
+					<img class='logo' src='images/logo.png' alt='gamin damali?' />
+					<span class='hometext'>Home</span>
+				</a>
+			</li>
+			<li class='navforum'>
+				<a class='navlink' href='#forum'>Forum</a>
+			</li>
+			<li class='navporn'>
+				<a class='navlink' href='#math'>Porn</a>
+			</li>
+			<li class='navdonate'>
+				<a class='navlink' href='#porn'>Donate</a>
+			</li>
+			<li class='navtext'>
+				Welcome to the home page of <span class="gavintext">Gavin DeMali</span> fan site!
+			</li>
+		</ul>
+		<div class='about-us'>
+			gavin demali was found in 1879 by charles darwin during a dredging operation in the mariana trench. the purpose of this expedition is difficult to pinpoint, but the most widely accepted hypothesis is that local reports of eel men sightings on the shores of guam encouraged darwin to investigate. what he found is currently classified by interpol. strikingly, we may have evidence to suggest that gavin demali belongs to an as-of-yet unclassified eel-ape population that survives in the deepest areas of the earth's oceans. eel men have a brain capacity five times that of a typical cisgender male homo sapien but 76% of neural activity is dedicated solely to an internal clock. <br />
+			petroleum-based materials (crude oil, plastics, etc.) constitute gavin's diet. <h1> Who are the eelman?</h1><br />
+			here are some archived images of eell males retrieved using FOIA requests:
+			<div class='home-figures'>
+				<figure style='width: 154.8px;'>
+					<img class='eelfetus' src='images/eelman-fetus.jpg' alt='oily' />
+					<figcaption style='font-size: 10px; font-family: Papyrus;'>Fig. 1 - Eel fetus</figcaption>
+				</figure>
+				<figure style='width: 88px;'>
+					<img class='adulteelman' src='images/artist-depiction-of-eelman.jpg' alt='artist depiction of eelman' />
+					<figcaption style='font-size: 10px; font-family: Papyrus;'>Fig. 2 - Artist depiction of adult eel man</figcaption>
+				</figure>
+			</div>
+			
+		</div>
+		<img class='dudelookin' src='images/lookin.jpg' alt='man looking' />
+	</body>
+</html>
